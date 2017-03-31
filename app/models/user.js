5c4eca635c6eedabf034cbc863ceef32a0ae9c83
Petros Papadopoulos <p.papadopoulos@soton.ac.uk>
2017-03-31 17:23:53 +0100

    Improvements on the forgot password functionality.

--- app/models/user.js
+++ app/models/user.js
@@ -30,6 +30,8 @@ var UserSchema = new Schema({
         type: String,
         required: true
     },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
     role: {
         admin: Boolean,
         user: Boolean,
